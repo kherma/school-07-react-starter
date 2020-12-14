03d55bbc54de5d19f88f3a5ae99a0b2b44fa2d3c
kherma <kamil.herma01@gmail.com>
2020-12-14 19:03:44 +0100

    Use dataStore.js data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
 import styles from "./App.scss";
 import List from "./../List/List.js";
+import { pageContents, listData } from "./../../data/dataStore.js";
 
 class App extends Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello react!</h2>
-        <List
-          title={["Things to do", <sup key="1">soon!</sup>]}
-          src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
-        >
-          <p>
-            God, please let me travel in time... oh wait, I just got this app...
-            so nevermind!!
-          </p>
-        </List>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        <List {...listData} />
       </main>
     );
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,22 +3,24 @@ import styles from "./List.scss";
 import Hero from "./../Hero/Hero.js";
 import PropTypes from "prop-types";
 import Column from "./../Column/Column.js";
+import { settings } from "./../../data/dataStore.js";
 
 class List extends Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
+    description: PropTypes.node,
+    columns: PropTypes.array,
     src: PropTypes.string,
   };
 
   static defaultProps = {
-    children: <p>Life is limitless with our app!!!</p>,
+    description: settings.defaultListDescription,
   };
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageSrc={this.props.src} />
-        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.description}>{this.props.description}</div>
         <div className={styles.columns}>
           <Column title="Animals" />
           <Column title="Plants" />
