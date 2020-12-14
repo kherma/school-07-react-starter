c62bf08854fb308751ebf964af43da4341af95ab
kherma <kamil.herma01@gmail.com>
2020-12-14 17:04:56 +0100

    Add Hero component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import styles from "./List.scss";
+import Hero from "./../Hero/Hero.js";
 
 class List extends Component {
   render() {
     return (
       <section className={styles.component}>
-        <h2>Things to do</h2>
+        <Hero />
       </section>
     );
   }
