77d7005b2ca7bc0e3679beda28d0951cce33f7c9
kherma <kamil.herma01@gmail.com>
2020-12-14 16:49:32 +0100

    Add List component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { Component } from "react";
+import React, { Component } from "react";
+import styles from "./App.scss";
+import List from "./../List/List.js";
 
 class App extends Component {
   render() {
     return (
-      <main className="app-content">
-        <h1>My first React app</h1>
-        <h2>Hello react!</h2>
+      <main className={styles.component}>
+        <h1 className={styles.title}>My first React app</h1>
+        <h2 className={styles.subtitle}>Hello react!</h2>
+        <List />
       </main>
     );
   }
---
+++ src/components/List/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./List.scss";
+
+class List extends Component {
+  render() {
+    return (
+      <section className={styles.component}>
+        <h2>Things to do</h2>
+      </section>
+    );
+  }
+}
+
+export default List;
