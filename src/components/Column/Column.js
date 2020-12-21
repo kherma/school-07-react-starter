c5e2de065bd180b17e3a8174879c100e7b6c3503
kherma <kamil.herma01@gmail.com>
2020-12-21 18:05:40 +0100

    Add column Creator component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from './../Card/Card.js';
 // import Creator from './../Creator/Creator.js';
-// import { settings } from './../../data/dataStore.js';
+import { settings } from './../../data/dataStore.js';
 import Icon from './../Icon/Icon.js';
 
 class Column extends Component {
@@ -14,6 +14,7 @@ class Column extends Component {
   };
 
   static defaultProps = {
+    icon: settings.defaultColumnIcon,
     title: 'Category',
   };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
-import {combineReducers, createStore} from 'redux';
+import { combineReducers, createStore } from 'redux';
 import initialStoreData from '../data/dataStore';
+import columnsReducer from './columnsRedux';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -11,10 +12,11 @@ const initialState = {
 
 // define reducers
 const reducers = {
+  columns: columnsReducer,
 };
 
 // add blank reducers for initial state properties without reducers
-Object.keys(initialState).forEach(item => {
+Object.keys(initialState).forEach((item) => {
   if (typeof reducers[item] == 'undefined') {
     reducers[item] = (statePart = null) => statePart;
   }
