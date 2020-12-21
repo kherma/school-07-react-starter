903f8ba0f48ea60f91faf22536d19070f1c97c3b
kherma <kamil.herma01@gmail.com>
2020-12-21 18:55:34 +0100

    Add card Creator component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from './../Card/Card.js';
-// import Creator from './../Creator/Creator.js';
+import Creator from './../Creator/Creator.js';
 import { settings } from './../../data/dataStore.js';
 import Icon from './../Icon/Icon.js';
 
@@ -11,6 +11,7 @@ class Column extends Component {
     title: PropTypes.node.isRequired,
     cards: PropTypes.array,
     icon: PropTypes.node,
+    addCard: PropTypes.func,
   };
 
   static defaultProps = {
@@ -19,7 +20,7 @@ class Column extends Component {
   };
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -32,11 +33,10 @@ class Column extends Component {
           {cards.map((cardData) => (
             <Card key={cardData.id} {...cardData} />
           ))}
-
-          {/* <Creator
+          <Creator
             text={settings.cardCreatorText}
-            action={(title) => this.addCard(title)}
-          /> */}
+            action={(title) => addCard(title)}
+          />
         </div>
       </section>
     );
