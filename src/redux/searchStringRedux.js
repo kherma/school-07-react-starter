713dfcf4ef5cc8c41df02add46d80e6fcce23fd3
kherma <kamil.herma01@gmail.com>
2020-12-22 15:03:04 +0100

    Finish Search component

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,5 +1,7 @@
+import shortid from 'shortid';
+
 // selectors
-export const getSearchString = (appState) => appState.searchString;
+export const getSearchString = ({ searchString }) => searchString;
 
 export const countVisibleCards = ({ cards, searchString }) =>
   cards.filter((card) => new RegExp(searchString, 'i').test(card.title)).length;
@@ -7,14 +9,24 @@ export const countVisibleCards = ({ cards, searchString }) =>
 export const countAllCards = ({ cards }) => cards.length;
 
 // action name creator
+const reducerName = 'searchString';
+const createActionName = (name) => `app/${reducerName}/${name}`;
 
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const createAction_changeSearchString = (payload) => ({
+  payload: payload,
+  id: shortid.generate(),
+  type: CHANGE,
+});
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
