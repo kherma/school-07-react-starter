7df5916d5d60da435d20622599f8562febc582b4
kherma <kamil.herma01@gmail.com>
2020-12-30 11:11:04 +0100

    Add Info component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
-import App from './components/App/AppContainer';
+import App from './components/App/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
